2b2b6635ee7fe473e312ac12e462b17bd25b21e9
David Williams <davwill@live.com>
2022-01-04 08:44:05 -0500

    new routes

--- routes/storeRouter.js
+++ routes/storeRouter.js
@@ -36,16 +36,15 @@ storeRouter.route('/')
     res.end('Delete operation not supported on /borrow');
 });
 
-storeRouter.route('/:storeId')
-.put((req, res, next) => {
-  Store.findByIdAndUpdate(req.params.storeId, {
-        $set: req.body
-    }) 
-    .then(store => {
-        console.log('Form entry created ', store);
+storeRouter.route('/:portalId')
+.options( (req, res) => { res.sendStatus(200); })
+.get(  (req, res, next) => {
+  Store.find({"portalId": req.params.portalId})
+    .then(prizes => {
+        console.log('Found ', prizes);
         res.statusCode = 200;
         res.setHeader('Content-Type', 'application/json');
-        res.json(store);
+        res.json({prizes, success: true});
     })
     .catch(err => next(err));
 });
