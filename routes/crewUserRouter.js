8b744c933b36a0f8059bfa6b3bd90ffadfe44f39
David Williams <davwill@live.com>
2022-02-03 07:06:41 -0500

    cmon

--- routes/crewUserRouter.js
+++ routes/crewUserRouter.js
@@ -300,32 +300,30 @@ crewUserRouter.route('/:userId')
       .catch(err => next(err));
   });
 
-crewUserRouter.route('/send/:userId')
+  crewUserRouter.route('/send/:userId')
   .put((req, res, next) => {
     CrewUser.findByIdAndUpdate(req.params.userId,
       {
         $push: { history: [req.body.history] },
         balance: req.body.balance
       },
-    )
-      .then(crewuser => {
-        CrewUser.findByIdAndUpdate(req.body.userId,
-          {
-            $push: { history: [req.body.history2] },
-            balance: req.body.balance2
-          },
-        )
-      })
-      .then(crewuser => {
-        console.log('History entry created ', crewuser);
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'application/json');
-        res.json({
-          crewuser,
-          success: true
-        });
-      })
-      .catch(err => next(err));
-  });
+    ).then(() => {
+  CrewUser.findByIdAndUpdate(req.body.userId,
+    {
+      $push: { history: [req.body.history2] },
+      balance: req.body.balance2
+    },
+  )})
+    .then(crewuser => {
+      console.log('History entry created ', crewuser);
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'application/json');
+      res.json({
+        crewuser,
+        success: true
+      });
+    })
+    .catch(err => next(err));
+});
 
 module.exports = crewUserRouter;
