c944a9acaa18100addb19adc8d86fde9721e5295
David Williams <davwill@live.com>
2022-02-20 17:33:55 -0500

    new store stuff

--- routes/announcementsRouter.js
+++ routes/announcementsRouter.js
@@ -3,52 +3,59 @@ const announcementsRouter = express.Router();
 const cors = require('./cors');
 const Announcements = require('../models/announcements');
 const authenticate = require('../authenticate');
+const CrewUser = require('../models/crewuser');
 
 
 
 announcementsRouter.route('/')
-.options( (req, res) => { res.sendStatus(200); })
-.get(  (req, res, next) => {
-  Announcements.find()
+  .options((req, res) => { res.sendStatus(200); })
+  .get((req, res, next) => {
+    Announcements.find()
       .then((announcements) => {
         res.statusCode = 200;
         res.setHeader('Content-Type', 'application/json');
-        res.json({announcements, success: true});
+        res.json({ announcements, success: true });
       })
       .catch((err) => next(err));
   })
   .post(cors.cors, (req, res, next) => {
-    Announcements.create(req.body)
-      .then (Announcements.find())
-      .then((announcements) => {
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'application/json');
-        res.json({announcements, success: true});
+    const portalId = req.body.portalId;
+    CrewUser.updateMany({ portalId: portalId }, { $set: { newAnnouncement: true } })
+      .then(() => {
+        console.log('updated')
+      }).then(() => {
+        Announcements.create(req.body)
+          .then(Announcements.find())
+          .then((announcements) => {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.json({ announcements, success: true });
+          })
       })
       .catch((err) => next(err));
   })
-  
-.put((req, res) => {
+
+  .put((req, res) => {
     res.statusCode = 403;
     res.end('PUT operation not supported on /borrow');
-})
-.delete((req, res) => {
+  })
+  .delete((req, res) => {
     res.statusCode = 403;
     res.end('Delete operation not supported on /borrow');
-});
+  });
 
 announcementsRouter.route('/:portalId')
-.options( (req, res) => { res.sendStatus(200); })
-.get(  (req, res, next) => {
-  Announcements.find({"portalId": req.params.portalId})
-    .then(announcements => {
+  .options((req, res) => { res.sendStatus(200); })
+  .get((req, res, next) => {
+    Announcements.find({ "portalId": req.params.portalId })
+      .then(announcements => {
         console.log('Found ', announcements);
         res.statusCode = 200;
         res.setHeader('Content-Type', 'application/json');
-        res.json({announcements, success: true});
-    })
-    .catch(err => next(err));
-});
+        res.json({ announcements, success: true });
+      })
+      .catch(err => next(err));
+  });
 
 announcementsRouter.route('/delete/:postId')
   .delete((req, res) => {
