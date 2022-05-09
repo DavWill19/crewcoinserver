4cbb0af9483bd323328e06fab279aaefa047d16b
David Williams <davwill@live.com>
2022-05-09 16:24:42 -0400

    updatestore

--- routes/storeRouter.js
+++ routes/storeRouter.js
@@ -19,80 +19,78 @@ storeRouter.route('/')
   })
   .post(cors.cors, (req, res, next) => {
     const portalId = req.body.portalId;
-    CrewUser.updateMany({portalId: portalId}, {$set: {newStoreItem: true}})
-    .catch((err) => next(err))
-    .then(() => {
-      console.log('updated')
-    }).then(() => {
-    Store.create(req.body)
+    CrewUser.updateMany({ portalId: portalId }, { $set: { newStoreItem: true } })
+      .catch((err) => next(err))
+      .then(() => {
+        console.log('updated')
+      }).then(() => {
+        Store.create(req.body)
           .then((store) => {
-            
+
             console.log('Form entry created ', store.store);
             res.statusCode = 200;
             res.setHeader('Content-Type', 'application/json');
             res.json({ store, success: true });
           })
-    })
-          .catch((err) => console.log(err));
       })
-        .put((req, res) => {
-        res.statusCode = 403;
-        res.end('PUT operation not supported on /borrow');
-      })
-      .delete((req, res) => {
-        res.statusCode = 403;
-        res.end('Delete operation not supported on /borrow');
-      });
+      .catch((err) => console.log(err));
+  })
+  .put((req, res) => {
+    res.statusCode = 403;
+    res.end('PUT operation not supported on /borrow');
+  })
+  .delete((req, res) => {
+    res.statusCode = 403;
+    res.end('Delete operation not supported on /borrow');
+  });
 
-    storeRouter.route('/:portalId')
-      .options((req, res) => { res.sendStatus(200); })
-      .get((req, res, next) => {
-        Store.find({ "portalId": req.params.portalId })
-          .then(prizes => {
-            console.log('Found ', prizes);
+storeRouter.route('/:portalId')
+  .options((req, res) => { res.sendStatus(200); })
+  .get((req, res, next) => {
+    Store.find({ "portalId": req.params.portalId })
+      .then(prizes => {
+        console.log('Found ', prizes);
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json({ prizes, success: true });
+      })
+      .catch(err => next(err));
+  });
+storeRouter.route('/:prizeId')
+  .put((req, res) => {
+    Store.findById(req.params.prizeId)
+      .then(prize => {
+        if (req.body.title) prize.title = req.body.title;
+        if (req.body.description) prize.description = req.body.description;
+        if (req.body.image) prize.image = req.body.image;
+        if (req.body.cost) prize.cost = req.body.cost;
+        prize.save()
+          .then(() => {
             res.statusCode = 200;
             res.setHeader('Content-Type', 'application/json');
-            res.json({ prizes, success: true });
+            res.json({ prize, success: true });
           })
+          .catch(err => next(err))
           .catch(err => next(err));
       });
-      storeRouter.route('/:prizeId')
-      .put((req, res) => {
-        Store.findById(req.params.prizeId)
-          .then(prize => {
-            if (req.body.title) prize.title = req.body.title;
-            if (req.body.description) prize.description = req.body.description;
-            if (req.body.image) prize.image = req.body.image;
-            if (req.body.cost) prize.cost = req.body.cost;
-            prize.save()
-          .then(prize => {
-            res.statusCode = 200;
-            res.setHeader('Content-Type', 'application/json');
-            res.json({
-              prize,
-              success: true
-            });
-          })
-          .catch(err => next(err));
-      });
+  });
+
+storeRouter.route('/:prizeId')
+  .delete((req, res) => {
+    Store.findByIdAndDelete(req.params.prizeId,
+      {
+        prizeId: req.body._id
+      }
+    )
+      .then(prizes => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json({
+          prizes,
+          success: true
+        });
       })
+      .catch(err => next(err));
+  });
 
-    storeRouter.route('/:prizeId')
-      .delete((req, res) => {
-        Store.findByIdAndDelete(req.params.prizeId,
-          {
-            prizeId: req.body._id
-          }
-        )
-          .then(prizes => {
-            res.statusCode = 200;
-            res.setHeader('Content-Type', 'application/json');
-            res.json({
-              prizes,
-              success: true
-            });
-          })
-          .catch(err => next(err));
-      });
-
-    module.exports = storeRouter;
+module.exports = storeRouter;
